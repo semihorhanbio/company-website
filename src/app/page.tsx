922d2c08c9ca3d67a6b16ff1b5a8906ffab0d9b3
Semih Orhan <sorhan3.14@gmail.com>
2024-01-04 16:35:36 +0300

    add Hero to other pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import homeImg from "public/home.png";
+import homeImg from "public/home.jpg";
 import Hero from "@/components/Hero";
 
 export default function Home() {
--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -1,3 +1,12 @@
+import performanceImg from "public/performance.jpg";
+import Hero from "@/components/Hero";
+
 export default function PerformancePage() {
-  return <div>Performance Page!</div>;
+  return (
+    <Hero
+      imgData={performanceImg}
+      imgAlt="welding"
+      title="We serve high performance applications"
+    />
+  );
 }
