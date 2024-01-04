6792177613c6e943297b8b82c6ae753f25717d0e
Semih Orhan <sorhan3.14@gmail.com>
2024-01-04 16:20:53 +0300

    add hero component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
 import homeImg from "public/home.png";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="car factory"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-    </div>
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
   );
 }
