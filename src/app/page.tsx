b1ff5eeef56db9910cc2f689169ce899b5e6ca25
Semih Orhan <sorhan3.14@gmail.com>
2024-01-04 13:20:23 +0300

    add links to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import Link from "next/link";
 
 export default function Home() {
   return ( 
-  <div>Home Page
-    <div>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
-    </div>
-  </div>
+  <div>Home Page</div>
   );
 }
