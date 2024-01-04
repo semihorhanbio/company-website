d23338b2a17ac52348fcb9b853336cfdf26840a3
Semih Orhan <sorhan3.14@gmail.com>
2024-01-04 13:11:14 +0300

    link between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return ( 
+  <div>Home Page
+    <div>
+      <Link href="/performance">Performance</Link>
+      <Link href="/reliability">Reliability</Link>
+      <Link href="/scale">Scale</Link>
+    </div>
+  </div>
+  );
 }
